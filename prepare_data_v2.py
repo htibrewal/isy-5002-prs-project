c457967da9757284d77928b216163937bc0ed43a
harsh <harshtibrewal18@gmail.com>
2024-10-21 16:53:19 +0800

    feat: make changes in prepare data to apply mean over 30 mins sampling

--- prepare_data_v2.py
+++ prepare_data_v2.py
@@ -9,30 +9,57 @@ from setup import fetch_data_from_subfolders, numerical_features, categorical_fe
 
 load_dotenv()
 
-def prepare_historical_parking_df_v2():
-    historical_parking_df = fetch_data_from_subfolders()
-
+def replace_timestamp_with_cyclic_features(target_df, timestamp_key):
     # feature engineering - updated timestamp
-    historical_parking_df['update_timestamp'] = pd.to_datetime(historical_parking_df['update_timestamp'])
+    target_df[timestamp_key] = pd.to_datetime(target_df[timestamp_key])
 
-    # get month, day_of_week, hour
-    historical_parking_df['month'] = historical_parking_df['update_timestamp'].dt.month
-    historical_parking_df['day_of_week'] = historical_parking_df['update_timestamp'].dt.weekday
-    historical_parking_df['hour'] = historical_parking_df['update_timestamp'].dt.hour
+    target_df['month'] = target_df[timestamp_key].dt.month
+    target_df['day_of_week'] = target_df[timestamp_key].dt.weekday
+    target_df['hour'] = target_df[timestamp_key].dt.hour
 
     # create cyclic features from month, day_of_week, hour
-    historical_parking_df['sin_hour'] = np.sin(2 * np.pi * historical_parking_df['hour'] / 24)
-    historical_parking_df['cos_hour'] = np.cos(2 * np.pi * historical_parking_df['hour'] / 24)
+    target_df['sin_hour'] = np.sin(2 * np.pi * target_df['hour'] / 24)
+    target_df['cos_hour'] = np.cos(2 * np.pi * target_df['hour'] / 24)
 
-    historical_parking_df['sin_day_of_week'] = np.sin(2 * np.pi * historical_parking_df['day_of_week'] / 7)
-    historical_parking_df['cos_day_of_week'] = np.cos(2 * np.pi * historical_parking_df['day_of_week'] / 7)
+    target_df['sin_day_of_week'] = np.sin(2 * np.pi * target_df['day_of_week'] / 7)
+    target_df['cos_day_of_week'] = np.cos(2 * np.pi * target_df['day_of_week'] / 7)
 
-    historical_parking_df['sin_month'] = np.sin(2 * np.pi * historical_parking_df['month'] / 12)
-    historical_parking_df['cos_month'] = np.cos(2 * np.pi * historical_parking_df['month'] / 12)
+    target_df['sin_month'] = np.sin(2 * np.pi * target_df['month'] / 12)
+    target_df['cos_month'] = np.cos(2 * np.pi * target_df['month'] / 12)
+
+    return target_df.drop(columns = [timestamp_key, 'month', 'day_of_week', 'hour'])
+
+
+def get_timestamp_mean_sampled_df(target_df, timestamp_key):
+    target_df[timestamp_key] = pd.to_datetime(target_df[timestamp_key])
+    target_df.set_index(timestamp_key, inplace=True)
+
+    target_reduced_df = (target_df
+                         .groupby('car_park_number')
+                         .resample('30T')
+                         .agg({'available_lots': 'mean'})
+                         .reset_index())
+    return target_reduced_df.dropna()
+
+
+def prepare_historical_parking_df_v2(use_mean_sampling = False):
+    historical_parking_df = fetch_data_from_subfolders()
 
     # drop not required features
-    historical_parking_df = historical_parking_df.drop(
-        columns=['fetch_timestamp', 'lot_type', 'update_timestamp', 'month', 'day_of_week', 'hour']
+    historical_parking_df = historical_parking_df.drop(columns=['fetch_timestamp', 'lot_type'])
+
+    if use_mean_sampling:
+        parking_lot_capacity_df = historical_parking_df.drop(columns=['available_lots', 'update_timestamp'])
+        parking_lot_capacity_df = parking_lot_capacity_df.groupby('car_park_number')['total_lots'].last().reset_index()
+
+        historical_parking_reduced_df = get_timestamp_mean_sampled_df(historical_parking_df, 'update_timestamp')
+
+        historical_parking_df = pd.merge(historical_parking_reduced_df, parking_lot_capacity_df, on='car_park_number', how='inner')
+
+    # replace timestamp with cyclic values for its features
+    historical_parking_df = replace_timestamp_with_cyclic_features(
+        historical_parking_df.copy(),
+        'update_timestamp'
     )
 
     print("Historical parking data shape = ", historical_parking_df.shape)
@@ -62,12 +89,12 @@ def prepare_parking_info_df_v2(folder_path = None):
     return pd.concat([parking_info_df, encoded_features], axis=1)
 
 
-def prepare_resultant_df_v2():
+def prepare_resultant_df_v2(use_mean_sampling = False):
     # fetch prepared car lot info (static)
     parking_info_df = prepare_parking_info_df_v2()
 
     # fetch prepared car parking data (historical)
-    historical_parking_df = prepare_historical_parking_df_v2()
+    historical_parking_df = prepare_historical_parking_df_v2(use_mean_sampling)
 
     # prepare a resultant DataFrame
     resultant_df = pd.merge(historical_parking_df, parking_info_df, on='car_park_number', how='inner')
