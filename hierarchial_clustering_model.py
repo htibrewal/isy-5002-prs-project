2257a5923cad8a6f68295f98e4b685adad21e10d
harsh <harshtibrewal18@gmail.com>
2024-10-22 12:12:44 +0800

    feat: add logic to save png file after clustering

--- hierarchial_clustering_model.py
+++ hierarchial_clustering_model.py
@@ -1,10 +1,11 @@
-from scipy.cluster.hierarchy import linkage, fcluster
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import linkage, fcluster, dendrogram
 from sklearn.metrics import silhouette_score
 
 from prepare_data_v2 import prepare_resultant_df_v2
 
 resultant_df = prepare_resultant_df_v2()
-resultant_df = resultant_df[:10000]
+# resultant_df = resultant_df[:10000]
 
 X = resultant_df.drop(columns=['car_park_number'])
 # y = resultant_df['car_park_number']
@@ -27,3 +28,11 @@ for k in range(2, 20):
 
 print(f"Best Silhouette score is {best_silhouette_score: .2f}")
 print(f"Best no of clusters is {best_k}")
+
+
+plt.figure(figsize=(10, 5))
+dendrogram(Z, labels=resultant_df['car_park_number'].values)
+plt.title('Hierarchical Clustering Dendrogram (Available Lots > 0)')
+plt.xlabel('Parking Lot')
+plt.ylabel('Distance')
+plt.savefig('hierarchical_clustering.png')
